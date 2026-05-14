5b25f01c0b00e3a680ac5cb193fe229ce6f79a8c
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2026-05-14 10:42:50 -0300

    chore: configure vite, styles, components, routing, protected routes, and linting rules

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center ">
+      <Button className="cursor-pointer bg-blue-600">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { ProtectedRoute } from './ProtectedRoute';
+import App from '../App.tsx';
+
+export const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  {
+    element: <ProtectedRoute />,
+    children: [{ path: '/profile', element: <div>profile</div> }],
+  },
+  { path: '*', element: <div> 404 Not Found</div> },
+]);
