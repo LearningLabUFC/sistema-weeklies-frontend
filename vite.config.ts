8aafa83624b6483427ba3970b0b0abd12f208802
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2026-05-24 12:23:46 -0300

    test: setup of vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
