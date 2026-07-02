35f36275cf90102475f54f4bbe97e5a64f73e649
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2026-07-02 19:41:15 -0300

    feat: add verify code page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,12 @@ import { ProtectedRoute } from './ProtectedRoute';
 import App from '../App.tsx';
 
 import { NotFound } from '@/pages/Not Found/NotFound.tsx';
+
 import Login from '@/pages/Login/Login.tsx';
 import Register from '@/pages/Register/Register.tsx';
+import ForgotPassword from '@/pages/ForgotPassword/ForgotPassword.tsx';
+import VerifyCode from '@/pages/VerifyCode/VerifyCode.tsx';
+import ResetPassword from '@/pages/ResetPassword/ResetPassword.tsx';
 
 export const router = createBrowserRouter([
   {
@@ -21,6 +25,18 @@ export const router = createBrowserRouter([
   {
     path: '/register',
     element: <Register />,
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPassword />,
+  },
+  {
+    path: '/verify-code',
+    element: <VerifyCode />,
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPassword />,
   },
   { path: '*', element: <NotFound /> },
 ]);
