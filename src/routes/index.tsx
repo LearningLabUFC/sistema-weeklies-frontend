c9028e3d5be3769f010beb76644b70b6ce50cba6
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2026-07-23 14:54:57 -0300

    refactor: organize route imports and simplify route structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
+
+import App from '@/App';
 import { ProtectedRoute } from './ProtectedRoute';
-import App from '../App.tsx';
 
-import { NotFound } from '@/pages/Not Found/NotFound.tsx';
+import { NotFound } from '@/pages/Not Found/NotFound';
 
-import Login from '@/pages/Login/Login.tsx';
-import Register from '@/pages/Register/Register.tsx';
-import ForgotPassword from '@/pages/ForgotPassword/ForgotPassword.tsx';
-import VerifyCode from '@/pages/VerifyCode/VerifyCode.tsx';
-import ResetPassword from '@/pages/ResetPassword/ResetPassword.tsx';
-import Profile from '@/pages/Profile/Profile.tsx';
-import Presence from '@/pages/Presence/Presence.tsx';
+import { authRoutes } from './auth.routes';
+import { userRoutes } from './user.routes';
+import { leaderRoutes } from './leader.routes';
+import { adminRoutes } from './admin.routes';
 
 export const router = createBrowserRouter([
   {
@@ -19,55 +17,15 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <App />,
-        children: [
-          {
-            index: true,
-            element: <div>Bem vindo ao Dashboard (Página Inicial)</div>,
-          },
-          {
-            path: '/profile',
-            element: <Profile />,
-          },
-          {
-            path: '/dashboard',
-            element: <div>Página do Dashboard</div>,
-          },
-          {
-            path: '/presenca',
-            element: <Presence />,
-          },
-          {
-            path: '/ponto',
-            element: <div>Página de bater ponto</div>,
-          },
-          {
-            path: '/weeklies',
-            element: <div>Minhas Weeklies</div>,
-          },
-        ],
+        children: [...userRoutes, ...leaderRoutes, ...adminRoutes],
       },
     ],
   },
 
+  ...authRoutes,
+
   {
-    path: '/login',
-    element: <Login />,
+    path: '*',
+    element: <NotFound />,
   },
-  {
-    path: '/register',
-    element: <Register />,
-  },
-  {
-    path: '/forgot-password',
-    element: <ForgotPassword />,
-  },
-  {
-    path: '/verify-code',
-    element: <VerifyCode />,
-  },
-  {
-    path: '/reset-password',
-    element: <ResetPassword />,
-  },
-  { path: '*', element: <NotFound /> },
 ]);
