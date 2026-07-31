513b3ca6b23fa486f4c35f83a660e7bf94cac721
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2026-07-31 13:20:59 -0300

    feat: add route for pending approval page in the router configuration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-
 import App from '@/App';
 import { ProtectedRoute } from './ProtectedRoute';
-
 import { NotFound } from '@/pages/Not Found/NotFound';
+import PendingApproval from '@/pages/user/PendingApproval/PendingApproval';
 
 import { authRoutes } from './auth.routes';
 import { userRoutes } from './user.routes';
@@ -15,15 +14,17 @@ export const router = createBrowserRouter([
     element: <ProtectedRoute />,
     children: [
       {
+        path: '/pending-approval',
+        element: <PendingApproval />,
+      },
+      {
         path: '/',
         element: <App />,
         children: [...userRoutes, ...leaderRoutes, ...adminRoutes],
       },
     ],
   },
-
   ...authRoutes,
-
   {
     path: '*',
     element: <NotFound />,
